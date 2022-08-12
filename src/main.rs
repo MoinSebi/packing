14d2830c6d8e6edde8e59649c3b20900eaa4b314
moinSebi <s.vorbrugg@googlemail.com>
2022-08-12 12:52:39 +0200

    Adjust the argument parser for easier understanding.

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,9 @@ fn main() {
             .about("Index a graph (gfa or VG pack)")
             .version("0.1.0")
             .setting(AppSettings::ArgRequiredElseHelp)
+            .help_heading("Input options")
             .arg(Arg::new("gfa")
+                .display_order(1)
                 .short('g')
                 .long("gfa")
                 .about("gfa for index")
@@ -79,6 +81,7 @@ fn main() {
                 .long("pack")
                 .about("pack format after alignment")
                 .takes_value(true))
+            .help_heading("Output options")
             .arg(Arg::new("output")
                 .short('o')
                 .long("output")
@@ -93,6 +96,8 @@ fn main() {
             .version("0.1.0")
             .setting(AppSettings::ArgRequiredElseHelp)
             // Input
+            .help_heading("Input options")
+
             .arg(Arg::new("pack")
                 .short('p')
                 .long("pack")
@@ -109,12 +114,7 @@ fn main() {
                 .about("Compressed pack file (only sequence). Original can only be accessed if the file is not normalized.")
                 .takes_value(true))
 
-            // Type
-            .arg(Arg::new("type")
-                .short('t')
-                .long("type")
-                .about("Type of output: nodes|sequence|pack (default: nodes)")
-                .takes_value(true))
+            .help_heading("Normalization parameters")
 
 
             // Modification
@@ -155,12 +155,21 @@ fn main() {
             // Alternative only one run per process
             // ReaderBit and u16 with stats function
             // You iterate and lose information directly
+            .help_heading("Output options")
+            // Type
+            .arg(Arg::new("type")
+                .short('t')
+                .long("type")
+                .about("Type of output: nodes|sequence|pack (default: nodes)")
+                .takes_value(true))
+
             .arg(Arg::new("out")
                 .short('o')
                 .long("out")
                 .about("Output name")
                 .default_value("pack")
                 .takes_value(true)))
+
 
 
         .get_matches();
