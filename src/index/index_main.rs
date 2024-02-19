56a02a27ab625b75f3b84ed7f1d27b85f4975698
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2024-02-19 13:21:24 +0100

    gfaR -> graph-reader

--- src/index/index_main.rs
+++ src/index/index_main.rs
@@ -1,4 +1,4 @@
-use gfaR_wrapper::{NGfa};
+use gfa_reader::{NCGfa};
 use log::info;
 use crate::helper::transform_u32_to_array_of_u8;
 
@@ -7,20 +7,20 @@ use crate::helper::transform_u32_to_array_of_u8;
 /// Same order than VG --> sort(node, sequence)
 pub fn make_index(filename: &str) -> Vec<u8>{
 
-    let mut graph = NGfa::new();
-    graph.from_graph(filename);
+    let mut graph: NCGfa<()> = NCGfa::new();
+    graph.parse_gfa_file(filename, false);
 
-    let mut nodes: Vec<u32> = graph.nodes.keys().cloned().collect();
-    println!("nn {}", nodes.len());
-    nodes.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    let mut nodes: Vec<_> = graph.nodes.iter().map(|u| (u.id, u.seq.len())).collect();
+    println!("Node length {}", nodes.len());
+    nodes.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
 
     let mut buf = Vec::new();
     let mut count = 0;
     for x in nodes.iter() {
-        for _y in 0..graph.nodes.get(x).unwrap().len {
+        for _y in 0..x.1 {
             count += 1;
-            buf.extend(transform_u32_to_array_of_u8(x.clone()));
+            buf.extend(transform_u32_to_array_of_u8(x.0.clone()));
         }
 
     }
