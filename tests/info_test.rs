e4d73026e8dd3c4c30bd7fd1298ba5cf564a4915
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2024-02-22 15:59:34 +0100

    Check test.

--- tests/info_test.rs
+++ tests/info_test.rs
@@ -11,36 +11,38 @@ fn info_pb() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd1 = Command::cargo_bin("packing")?;
     cmd1.arg("info").arg("-b").arg("data/example/9986.1k.pb");
     cmd1.assert()
-        .stdout(predicate::str::contains("Number of elements: 1"));
+        .stderr(predicate::str::contains("Entry type: Node"));
     cmd1.unwrap().assert().success();
 
     Ok(())
 }
 
+#[test]
 fn info_gfa() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd1 = Command::cargo_bin("packing")?;
-    cmd1.arg("info").arg("-pi").arg("data/example/9986.100k.pi");
+    cmd1.arg("info").arg("-i").arg("data/example/9986.1k.pi");
     cmd1.assert()
-        .stdout(predicate::str::contains("Number of nodes: 67"));
+        .stderr(predicate::str::contains("Number of nodes: 67"));
     cmd1.assert()
-        .stdout(predicate::str::contains("Number of entries: 999"));
+        .stderr(predicate::str::contains("Number of entries: 999"));
     cmd1.unwrap().assert().success();
     Ok(())
 }
 
+#[test]
 fn info_pt_bin() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd1 = Command::cargo_bin("packing")?;
     cmd1.arg("info")
-        .arg("-p")
+        .arg("-b")
         .arg("data/example/9986.1k.a1.bin.pt");
     cmd1.unwrap().assert().success();
     Ok(())
 }
 
+#[test]
 fn info_pt_u16() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd1 = Command::cargo_bin("packing")?;
-    cmd1.arg("info").arg("-p").arg("data/example/9986.1k.a1.pt");
+    cmd1.arg("info").arg("-b").arg("data/example/9986.1k.a1.pt");
     cmd1.unwrap().assert().success();
-
     Ok(())
 }
