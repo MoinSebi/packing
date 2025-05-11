c8af36e416ffbad38da9e19f59b31a036e495a1e
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2025-05-11 18:44:06 +0300

    Fixed error, adjusted to latest gfa-reader version.

--- src/index/index_wrapper.rs
+++ src/index/index_wrapper.rs
@@ -1,14 +1,13 @@
-use gfa_reader::NCGfa;
+use gfa_reader::Gfa;
 use log::info;
 use packing_lib::normalize::helper::transform_u32_to_array_of_u8;
 
 /// Read GFA and get nodes + sequences
 /// Same order than VG --> sort(node, sequence)
 pub fn make_index(filename: &str) -> Vec<u8> {
-    let mut graph: NCGfa<()> = NCGfa::new();
-    graph.parse_gfa_file(filename, false);
+    let mut graph: Gfa<u32, (), ()> = Gfa::parse_gfa_file_multi(filename, 1);
 
-    let mut nodes: Vec<_> = graph.nodes.iter().map(|u| (u.id, u.seq.len())).collect();
+    let mut nodes: Vec<_> = graph.segments.iter().map(|u| (u.id, u.sequence.get_len())).collect();
     println!("Node length {}", nodes.len());
     nodes.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
